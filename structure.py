6d5387ca6e3f3f803b4320bef79ec726956012cf
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2022-02-11 12:43:24 +0800

    successfully listed all variables that corresponds to json file

--- structure.py
+++ structure.py
@@ -16,12 +16,37 @@
 import json
 from fpdf import FPDF
 
-pdf= FPDF('P', 'mm', 'Letter')
-pdf.add_page()
-pdf.image('bg.png',x = -0.5, y= -0.5, w = pdf.w + 1)
+with open('personaldeets.json') as jsonFILE:
+    resumedata= json.load(jsonFILE)
 
-jsonPDF = open('personaldeets.json', 'r')
-read = jsonPDF.read()
-infos = json.loads(read)
+name= resumedata["complete_name"]
+gender= resumedata["gender_sex"]
+age= resumedata["age"]
+address= resumedata["home_address"]
+primaryeducation= resumedata["primaryeducation_basic"]
+secondaryeducation= resumedata["secondaryeducation_middle"]
+tertiaryeducation= resumedata["tertiaryeducation_high school"]
+college= resumedata["college"]
+masteraldegree= resumedata["masteral_degree"]
+achievement1= resumedata["achievement1"]
+achievement2= resumedata["achievement2"]
+achievement3= resumedata["achievement3"]
+achievement4= resumedata["achievement4"]
+achievement5= resumedata["achievement5"]
+achievement6= resumedata["achievement6"]
+skills1= resumedata["skills1"]
+skills2= resumedata["skills2"]
+skills3= resumedata["skills3"]
+skills4= resumedata["skills4"]
+skills5= resumedata["skills5"]
+skills6= resumedata["skills6"]
+mobnumber= resumedata["mobile_number"]
+emailaddress= resumedata["email_address"]
+telnumber= resumedata["telephone_number"]
+title=resumedata['title']
 
-pdf.output('VELASQUEZ_DANIELLAFRANCINE.pdf')
+resume= FPDF('P', 'mm', 'Letter')
+resume.add_page()
+resume.image('bg.png', x = -0.5, y= -0.5, w = resume.w + 1)
+
+resume.output('VELASQUEZ_DANIELLAFRANCINE.pdf')
